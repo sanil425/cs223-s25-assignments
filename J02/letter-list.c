e682958cf8b82fbf5ec75695ab0ccb877289c0aa
Sanil Kagalwala <skagalwala@goldengate.cs.brynmawr.edu>
2025-02-18 16:51:55 -0500

    J02 in class work

--- J02/letter-list.c
+++ J02/letter-list.c
@@ -1,4 +1,68 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    char letter;
+    struct Node* next;
+} Node;
+
+
+void insertFront (char input, Node** head) {
+      Node* newNode = (Node*)malloc(sizeof(Node));
+      if (newNode == NULL) {
+          printf("Error: out of space\n");
+          exit(1);
+     }
+      newNode->letter = input;
+      newNode->next = *head;
+      *head = newNode;
+}
+
+void printList (int size, Node** head) {
+    Node* ptr = *head;
+    while (ptr != NULL) {
+
+        printf("%c", ptr->letter);
+        ptr = ptr->next;
+    }
+}
+
+void clear (Node* head) {
+    if (head == NULL) {
+        return;
+    }
+    Node* nextNode = (head)->next;
+    free(head);
+    clear(nextNode);
+}
+
+
 int main()
 {
+    char* word = malloc(sizeof(char) * 32);
+    printf("Enter a word: ");
+    scanf("%s", word);
+    int len = strlen(word);
+    Node* head = NULL;
+
+    printf("Enter a character to remove: ");
+    char remove;
+    scanf(" %c", &remove);
+
+    for (int i = len - 1; i >= 0; i--) {
+        if (word[i] != remove) {
+
+            insertFront(word[i], &head);
+
+        }   
+    }
+    printList(len, &head);
+    printf("\n");
+    
+    free(word);
+    clear(head);
+
   return 0;
 }
+
--- J02/strblock.c
+++ J02/strblock.c
@@ -1,4 +1,37 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 int main()
 {
+    printf("Enter a string: ");
+    char* word = malloc(sizeof(char) * 32);
+    scanf("%s", word);
+    int len = strlen(word);
+    char* matrix = malloc(sizeof(char) * len * len);
+    
+    for (int i = 0; i < len * len; i++) {
+        matrix[i] = ' ';    
+
+    }
+
+    for (int i = 0; i < len; i++) {
+        matrix[i] = word[i];
+        matrix[(len * (len - 1)) + i] = word[i];
+        matrix[(len + 1) * i] = word[i];
+    }
+    
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len; j++) {
+            printf("%c ", matrix[(len * i) + j]);
+        }
+        printf("\n");
+    }
+    free(matrix);
+    free(word);
+
+        
   return 0;
 }
+
+
