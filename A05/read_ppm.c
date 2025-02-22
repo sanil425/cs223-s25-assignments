d1a26428e376cec0c77a106a0fdd820c4ef91903
Sanil Kagalwala <skagalwala@goldengate.cs.brynmawr.edu>
2025-02-22 18:06:16 -0500

    incomplete assignment

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -1,7 +1,9 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
+ * Author: Sanil Kagalwala
+ * Date: 20th February 2025
  * Description
+ * Implements function that can read PPM files
+ * stored in binary format.
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,5 +17,63 @@ struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  return NULL;
+  FILE* file = fopen(filename, "rb");
+  // Read file in and ensure read correctly
+  if (!file) { 
+    printf("Error opening file %s\n", filename);
+    return NULL;
+  }
+
+  // Read the PPM header P6
+  char magic_num[3];
+  char buffer[256]; // temporary buffer for reading lines
+
+  // Read first line for magic number
+  fgets(buffer, sizeof(buffer), file); // Read first line into buffer
+  sscanf(buffer, "%2s", magic_num); // Extract 2 numbers (P6)
+  magic_num[2] = '\0'; // ensure there is a null terminator
+
+  // read next line (but check if its a comment)
+  fgets(buffer, sizeof(buffer), file); // read next line into buffer
+  while (buffer[0] == '#') {
+    fgets(buffer, sizeof(buffer), file); // read next line if comment
+  }
+
+  sscanf(buffer, "%d %d", w, h); // Assign values to pointers w and h
+  
+  fgets(buffer, sizeof(buffer), file); // Read next line and read in max colour
+  int max_colour_val;
+  sscanf(buffer, "%d", &max_colour_val);
+
+  fgetc(file); // consumes the proceeding whitespace
+
+  // Create matrix pixels which is a double pointer
+  // malloc allocates memory for array of h pointers
+  struct ppm_pixel** pixels = malloc((*h) * sizeof(struct ppm_pixel*));
+  if (pixels == NULL) {
+    fclose(file);
+    return NULL; // Return NULL if memory allocation fails
+  // Allocates a row of w pixels. pixel[i] stores pointer to array of w pixels
+  for (int i = 0; i < *h; i++) {
+    pixels[i] = malloc((*w) * sizeof(struct ppm_pixel));
+    // Check if memory allocation fail, if so free all
+    if (pixels[i] == NULL) {
+      for (int j = 0; j < i; j++) {
+        free(pixels[j]);
+      }
+      free(pixels);
+      fclose(file);
+      return NULL;
+    }
+  }
+
+  // For each row of pixels, reads w number of info from ppm_pixel
+  // fread reads raw binary so use to read in data
+  for (int i = 0; i < *h; i++) {
+    fread(pixels[i], sizeof(struct ppm_pixel), *w, file);
+  }
+	
+  fclose(file);
+
+  return pixels; // return pointer to 2d array created
 }
--- A05/test_read.c
+++ A05/test_read.c
@@ -9,10 +9,26 @@
 
 int main() {
   int w, h;
-  struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
+  struct ppm_pixel** pixels = read_ppm_2d("feep-raw.ppm", &w, &h);
 
-  // todo: print out the grid of pixels
+  if (pixels == NULL) {
+    printf("Error reading PPM file\n");
+    return 1;
+  }
+  
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      printf("(%d,%d,%d) ", pixels[i][j].red, pixels[i][j].green,
+ 	      pixels[i][j].blue);
+    } 
+    printf("\n");
+  }
+  // User has to free allocated memory
+  for (int i = 0; i < h; i++) {
+    free(pixels[i]);
+  }
   free(pixels);
+
   return 0;
 }
 
