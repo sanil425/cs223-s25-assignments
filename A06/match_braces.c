5ca563b937a3dd1acdbae78ff08836357fdb1d17
Sanil Kagalwala <skagalwala@goldengate.cs.brynmawr.edu>
2025-03-01 19:51:25 -0500

    assignment complete

--- A06/match_braces.c
+++ A06/match_braces.c
@@ -1,7 +1,126 @@
+/*************************************************** 
+ * Author: Sanil Kagalwala
+ * Date: 27th February 2025
+ * Program implements that takes a C file as a command 
+ * line argument and uses a stack to check for matched 
+ * braces. If not all braces are matched, print the line 
+ * where a mis-matched brace occurs.
+ ***************************************************/
+
+#include <stdlib.h>
 #include <stdio.h>
 
-int main()
-{
+typedef struct Node {
+    int line;
+    int column;
+    struct Node* next;
+} Node;
+
+Node* pop(Node* stack, int* line, int* column) {
+  if (stack == NULL) {
+    return NULL; // If stack is empty return NULL
+  }
+  Node* top = stack; // current top of stack
+  *line = top->line; // Retrieve line number
+  *column = top->column; // Retrieve column number
+
+  stack = top->next; // Move the stack pointer to the next node from the top
+  free(top); // Free memory of old top
+
+  return stack;
+}
+
+Node* push(Node* stack, int line, int column) {
+  // Allocate memory for new node
+  Node* new_node = malloc(sizeof(Node));
+  if (new_node == NULL) {
+    printf("Memory allocation failed");
+    exit(1);
+  }
+  // Update data for new_node
+  new_node->line = line;
+  new_node->column = column;
+  new_node->next = stack;
+
+  return new_node;
+}
+
+void clear(Node* stack) {
+  while (stack != NULL) {
+    Node* curr = stack;  // Save a pointer to current node
+    stack = stack->next; // Move stack to next node
+    free(curr); // Free memory of current
+  }
+}
+
+void print(Node* stack) {
+  while (stack != NULL) {
+    printf("Brace at Line = %d, Column = %d\n", stack->line, stack->column);
+    stack = stack->next;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  // If number of arguments is not 2 (e.g match_braces prog.c),
+  // exit with error.
+  if (argc != 2) {
+    printf("Error reading command line args");
+    return 1;
+  }
+
+  // Opening the file from 2nd cmd line argument 
+  FILE* file = fopen(argv[1], "r");
+  if (file == NULL) {
+    printf("Error opening file");
+    return 1;
+  }
+
+  Node* stack = NULL; // Create the stack
+  int line = 1; // Track line and columns
+  int column = 0;
+
+  // Reading character by character using fgetc
+  int c;
+  while ((c = fgetc(file)) != EOF) {
+    column++; // Every character moves column forward
+    
+    if (c == '\n') {
+      line++;
+      column = 0; // Reset column at every new line
+    }
+    else if (c == '{') {
+      // If a { is found push onto stack
+      stack = push(stack, line, column);
+    }
+    // If } is found
+    else if (c == '}') {
+      int close_line, close_column;
+      if (stack != NULL) {
+	// If the stack isnt empty, pop most recent { from stack
+        stack = pop(stack, &close_line, &close_column);
+	// That { is the one matching the current }
+	// We print column numbers of { and }
+	printf("Found matching braces: (%d, %d) -> (%d, %d)\n",
+		close_line, close_column, line, column);
+      }
+      // If theres no { on the stack (the snack is NULL), the brace is unclosed
+      else {
+        printf("Unmatched brace on Line %d and Column %d\n", line, column);
+      }
+    }
+  }
+  
+  // Check for unmatched { in stack
+  while (stack != NULL) {
+   // If stack is not NULL there is a remaining { that hasn't been popped
+   int unclosed_line, unclosed_column;
+   stack = pop(stack, &unclosed_line, &unclosed_column);
+   printf("Unmatched brace on line %d and Column %d\n", unclosed_line, unclosed_column);
+  }
+
+  fclose(file); // Close file
+  clear(stack); // Clear all memory
+  
   return 0;
 }
 
--- A06/tree.c
+++ A06/tree.c
@@ -1,26 +1,96 @@
 #include "tree.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include <string.h>
 
-struct tree_node* find(const char* name, struct tree_node* root)
-{
-  return NULL;
+struct tree_node* helperCreateNode(const char* name) {
+  struct tree_node* new_node = malloc(sizeof(struct tree_node));
+  if (new_node == NULL) {
+    printf("Error with memory allocation");
+    exit(1);
+  }
+  strcpy(new_node->data.name, name); // Copy name into new node
+  new_node->left = NULL;
+  new_node->right = NULL;
+  return new_node;
 }
 
-struct tree_node* insert(const char* name, struct tree_node* root)
-{
-  return NULL;
+struct tree_node* insert(const char* name, struct tree_node* root) {
+  // Base case when root is NULL and we create new node
+  if (root == NULL) {
+    return helperCreateNode(name);
+  }
+  int compare = strcmp(name, root->data.name);
+  if (compare < 0) {
+    // name is less than roots name, so go left
+    root->left = insert(name, root->left);
+  }
+  else if (compare > 0) {
+    // name is more than roots name, so go right
+    root->right = insert(name, root->right);
+  }
+  return root;
 }
 
-void clear(struct tree_node* root)
-{
+struct tree_node* find(const char* name, struct tree_node* root) {
+  // Base case is when tree is empty or reached leaf with no match
+  if (root == NULL) return NULL;
+
+  // Compare name with current nodes name
+  int compare = strcmp(name, root->data.name);
+  // if names match
+  if (compare == 0) {
+    return root;
+  }
+  else if (compare < 0) {
+    // go to left subtree as smaller
+    return find(name, root->left);
+  }
+  else {
+    // right subtree
+    return find(name, root->right);
+  }
 }
 
-void print(struct tree_node* root)
-{
+void clear(struct tree_node* root) {
+  if (root == NULL) {
+    return; // tree is empty, functions job is done
+  }
+  // recursively clear left and right subtrees and then free current node
+  clear(root->left);
+  clear(root->right);
+  free(root);
 }
 
-void printSorted(struct tree_node* root)
-{
+void helperPrint(struct tree_node* root, int depth, char childType) {
+  // Base case
+  if (root == NULL) return;
+
+  // Draw tree
+  // Indent base on depth
+  for (int i = 0; i < depth; i++) {
+    printf("   ");
+  }
+  // Print root or children with l or r labels
+  if (depth == 0) printf("%s\n", root->data.name);  // print node
+  else printf("%c:%s\n", childType, root->data.name);  // l: or r:
+
+  // Recursivey call for left and right children chaning the level and child type
+  helperPrint(root->left, depth + 1, 'l');
+  helperPrint(root->right, depth + 1, 'r');
+  
+}
+
+void print(struct tree_node* root) {
+  helperPrint(root, 0, ' ');  // Start at depth 0 with no child label
+}
+
+void printSorted(struct tree_node* root) {
+  if (root == NULL) {
+    return;
+  }
+  printSorted(root->left);
+  printf("%s\n", root->data.name);
+  printSorted(root->right);
 }
 
