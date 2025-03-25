fcecfd36039bac64feb4e620fd8a236a63c5711e
Sanil Kagalwala <skgalwala@haverford.edu>
2025-03-25 16:31:38 -0400

    j04 complete

--- J04/message_reader.c
+++ J04/message_reader.c
@@ -1,4 +1,41 @@
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct meta_data {
+  float version;
+  unsigned int year;
+  unsigned int length;
+};
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    printf("Error: Please enter a file");
+    exit(1);
+  }
+  FILE *filePointer;
+  char header[1024];
+  filePointer = fopen(argv[1], "rb");
+  if (!filePointer) {
+    printf("File could not be opened\n");
+    exit(1);
+  }
+  fgets(header, 20, filePointer);
+  printf("Comment: %s\n", header);
+  struct meta_data* data = (struct meta_data*)malloc(sizeof(struct meta_data));
+  fread(data, 4, 3, filePointer);
+  printf("Version: %.2f \n", data->version);
+  printf("Year: %d \n", data->year);
+  printf("Length: %d \n", data->length);
+  fgets(header, 1024, filePointer);
+  printf("Message: %s", header);
+  if (data->length > 1024) {
+    printf("... etc");
+  }
+  else {
+    printf("\n");
+  }
+  free(data);
+  fclose(filePointer);
   return 0;
 }
--- J04/message_writer.c
+++ J04/message_writer.c
@@ -1,4 +1,27 @@
-int main()
-{
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    printf("usage: ./message_writer filename");
+    return(1);
+  }
+  FILE *filePointer;
+  filePointer = fopen(argv[1], "wb");
+  if (!filePointer) {
+    printf("File could not be opened\n");
+    return(1);
+  }
+  char* comment = "# Carl Sagan\n";
+  float version = 0.10;
+  int year = 1990;
+  char* data = "Look again at that dot. That's here. That's home. That's us. On it is everyone you love, everyone you know.";
+  int length = strlen(data);
+  fwrite(comment, strlen(comment), 1, filePointer);
+  fwrite(&version, sizeof(float), 1, filePointer);
+  fwrite(&year, sizeof(int), 1, filePointer);
+  fwrite(&length, sizeof(int), 1, filePointer);
+  fwrite(data, strlen(data) + 1, 1, filePointer);
   return 0;
 }
