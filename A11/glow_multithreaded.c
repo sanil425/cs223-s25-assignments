ee5a8752f512d05d9fb74f7c3621c944d413984e
alinen <anormoyle@brynmawr.edu>
2025-04-05 12:01:45 -0400

    update command line parameters

--- A11/glow_multithreaded.c
+++ A11/glow_multithreaded.c
@@ -7,32 +7,23 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 
-int main(int argc, char* argv[]) {
-  int size = 480;
-  float xmin = -2.0;
-  float xmax = 0.47;
-  float ymin = -1.12;
-  float ymax = 1.12;
-  int maxIterations = 1000;
-  int numProcesses = 4;
+int main(int argc, char* argv[]) 
+{
+  int N = 4;
+  int threshold = 200;
+  int blursize = 24;
+  const char* filename = "earth-small.ppm";
 
   int opt;
-  while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
+  while ((opt = getopt(argc, argv, ":N:t:b:f:")) != -1) {
     switch (opt) {
-      case 's': size = atoi(optarg); break;
-      case 'l': xmin = atof(optarg); break;
-      case 'r': xmax = atof(optarg); break;
-      case 't': ymax = atof(optarg); break;
-      case 'b': ymin = atof(optarg); break;
-      case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> "
-        "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
+      case 'N': N = atoi(optarg); break;
+      case 't': threshold = atof(optarg); break;
+      case 'b': blursize = atof(optarg); break;
+      case 'f': filename = optarg; break;
+      case '?': printf("usage: %s -N <NumThreads> -t <brightness threshold> -b <box blur size> -f <ppmfile>\n", argv[0]); break;
     }
   }
-  printf("Generating mandelbrot with size %dx%d\n", size, size);
-  printf("  Num processes = %d\n", numProcesses);
-  printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
-  printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
-
   // todo: your code here
   // compute image
 }
