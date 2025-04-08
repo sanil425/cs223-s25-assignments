e6f88e0e6dfb2e3db461cd23dc4c9ca926894138
Sanil Kagalwala <skgalwala@haverford.edu>
2025-04-08 16:40:03 -0400

    sanil and lola J05 submission

--- J05/dot.c
+++ J05/dot.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <time.h>
-#include <sys/time.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #define SIZE 100000
+#define NUM_THREADS 4
+int sums_array[4];
 
-// your code here
+struct thread_data {
+  int start_index;
+  int end_index;
+  int *v;
+  int *u;
+  int partial_sum;
+};
+
+int v[SIZE];
+int u[SIZE];
+struct thread_data data[NUM_THREADS];
+
+void *compute_dot(void *userdata) {
+  struct thread_data *data = (struct thread_data *) userdata;
+  int sum = 0;
+  for (int i = data->start_index; i < data->end_index; i++) {
+    sum += data->v[i] * data->u[i];
+  }
+  data->partial_sum = sum;
+  return NULL;
+}
 
 int main(int argc, char *argv[]) {
   srand(time(0));
-
-  int v[SIZE];
-  int u[SIZE];
   int dotproduct = 0;
-   
+
   for (int i = 0; i < SIZE; i++) {
     v[i] = rand() % 1000 - 500;
     u[i] = rand() % 1000 - 500;
@@ -22,11 +41,26 @@ int main(int argc, char *argv[]) {
   }
   printf("Ground truth dot product: %d\n", dotproduct);
 
-  // TODO: Implement your thread solution here
+  // our solution here
+  pthread_t threads[NUM_THREADS];
+  int chunk = SIZE / NUM_THREADS;
+
+  for (int i = 0; i < NUM_THREADS; i++) {
+    data[i].start_index = i * chunk;
+    data[i].end_index = (i == NUM_THREADS - 1) ? SIZE : (i + 1) * chunk;
+    data[i].v = v;
+    data[i].u = u;
+    data[i].partial_sum = 0;
+    pthread_create(&threads[i], NULL, compute_dot, &data[i]);
+  }
+
   int thread_dotproduct = 0;
+  for (int i = 0; i < NUM_THREADS; i++) {
+    pthread_join(threads[i], NULL);
+    thread_dotproduct += data[i].partial_sum;
+  }
+
   printf("Test with 4 threads\n");
-
   printf("Answer with threads: %d\n", thread_dotproduct);
-
   return 0;
 }
--- J05/multiply.c
+++ J05/multiply.c
@@ -5,8 +5,30 @@
 #include <pthread.h>
 
 #define SIZE 100
+#define NUM_THREADS 4
+#define CHUNK (SIZE / NUM_THREADS)
 
 // your code here
+struct thread_data {
+  int submatrix[CHUNK][SIZE];
+  int *vector;
+  int *result;
+  int offset;  // where to start writing in result array
+};
+
+
+void *matrix_vector_product(void *arg) {
+  struct thread_data *data = (struct thread_data *) arg;
+  for (int i = 0; i < CHUNK; i++) {
+    int sum = 0;
+    for (int j = 0; j < SIZE; j++) {
+      sum += data->submatrix[i][j] * data->vector[j];
+    }
+    data->result[data->offset + i] = sum;
+  }
+  return NULL;
+}
+
 
 int main(int argc, char *argv[]) {
   srand(time(0));
@@ -32,7 +54,27 @@ int main(int argc, char *argv[]) {
   // TODO: Implement your thread solution here
   printf("Test with 4 threads\n");
 
+  pthread_t threads[NUM_THREADS];
+  struct thread_data thread_args[NUM_THREADS];
 
+  // copy 25 row sub matrix into thread data
+  for (int t = 0; t < NUM_THREADS; t++) {
+    int base = t * CHUNK; // where on M to start from
+    for (int i = 0; i < CHUNK; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        thread_args[t].submatrix[i][j] = M[(base + i) * SIZE + j];
+        // convert into 2d array from 1d array M
+      } 
+    }
+    thread_args[t].vector = u;
+    thread_args[t].result = result_threads;
+    thread_args[t].offset = base;
+    pthread_create(&threads[t], NULL, matrix_vector_product, &thread_args[t]);
+  }
+
+  for (int i = 0; i < NUM_THREADS; i++) {
+    pthread_join(threads[i], NULL);
+  }
 
   int error = 0;
   for (int i = 0; i < SIZE; i++) {
